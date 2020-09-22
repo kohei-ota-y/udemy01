35fdd874dc4315dac37ffed368ff14c006319384
ota kohei <8.8.kouhei.8.8@gmail.com>
2020-09-22 23:42:31 +0900

    コンポーネント

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ function App() {
     <React.Fragment>
     <label htmlFor="bar">bar</label>
       <input id="bar" type="text" onChange={() => {console.log('aaa')}}/>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </React.Fragment>
   );
 }
-
+const Cat = () => {
+  return(
+  <React.Fragment>
+    <p>Meow!</p>
+  </React.Fragment>
+  )
+}
 export default App;
