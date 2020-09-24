02d472aa893bf75ec40da311830559e2a8e59d26
ota kohei <8.8.kouhei.8.8@gmail.com>
2020-09-24 18:25:18 +0900

    eventsファイルの作成。 axios,redux-thunkの導入　 actionへreadEvantsをdispatchする処理を記述

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore , applyMiddleware} from 'redux'
 //作成したストアーを全コンポーネントに渡すための機能を持つプロバイダー
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import './index.css';
 import reducer from './reducers'
 
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer)
+const store = createStore(reducer ,applyMiddleware(thunk))
 //application内の全てのstoreはここに集約される
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+    <EventsIndex />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 //全reducerを纏める役割のjs
 import { combineReducers } from 'redux'
-import count from './count'
+import events from './events'
 
 //storで使用するために
-export default combineReducers({ count })
+export default combineReducers({ events })
 //複数例 export default combineReducers({ foo, bar, baz })
