74296a98c60c8364e680733d028f3be7c63881a9
ota kohei <8.8.kouhei.8.8@gmail.com>
2020-09-24 18:53:52 +0900

    イベント一覧のtableを表示させる処理 lodashのimport

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,20 @@
 import { READ_EVENTS } from '../actions'
+import _ from  'lodash'
 
-export default (state = {},action) => {
+export default (events = {},action) => {
   switch (action.type) {
     case READ_EVENTS:
-      return state
+//[
+//  {id: 1, title: "Let's have an event 1!", body: "This is the body for event 1."}
+//  {id: 2, title: "Let's have an event 2!", body: "This is the body for event 2."}
+//]
+//↓変換したい(lodashからインポートし、_.mapKeys(action.response.data, 'id')記述する)
+//{
+//  0: {id: 1, title: "Let's have an event 1!", body: "This is the body for event 1."}
+//  1: {id: 2, title: "Let's have an event 2!", body: "This is the body for event 2."}
+//}
+      return _.mapKeys(action.response.data, 'id')
     default:
-      return state
+      return events
   }
 }
