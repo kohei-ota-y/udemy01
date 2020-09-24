b75e6b24b42deed824a52534e0ff0ff6d0920b01
ota kohei <8.8.kouhei.8.8@gmail.com>
2020-09-24 11:35:52 +0900

    redux　react-reduxの導入 アクションクリエイターの作成

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+//action creater
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type:INCREMENT
+})
+export const decrement = () => ({
+  type:DECREMENT
+})
