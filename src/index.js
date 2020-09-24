a121cb8e23fb467c0fc5ecaf21088e8c7ad4b1bf
ota kohei <8.8.kouhei.8.8@gmail.com>
2020-09-24 12:08:05 +0900

    store について

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+//作成したストアーを全コンポーネントに渡すための機能を持つプロバイダー
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducer'
+
+import App from './component/App';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(reducer)
+//application内の全てのstoreはここに集約される
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+    //アプリケーション内のどのコンポーネントからもstoreに参照できるようにする
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
