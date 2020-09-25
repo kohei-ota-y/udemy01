6c8af354607a9b3f4486bfad1f29f5c59fde4169
ota kohei <8.8.kouhei.8.8@gmail.com>
2020-09-25 11:06:56 +0900

    APIサーバーに対してイベントの作成要求を送信してイベントを作成する

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 //全reducerを纏める役割のjs
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
 //storで使用するために
-export default combineReducers({ events })
+export default combineReducers({ events , form})
 //複数例 export default combineReducers({ foo, bar, baz })
